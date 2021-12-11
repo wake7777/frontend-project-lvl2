d7802de3f4e8f91430e8db8fa98d2cfc923fa986
Alexander Trusov <sanektttt@gmail.com>
2021-12-11 19:02:43 +0400

    first commit 1-2 slide

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]',   'output format')
+  .arguments('<filepath1> <filepath2>')
+program.parse();
+
---
+++ makefile
@@ -0,0 +1,11 @@
+install:
+	npm ci
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
+
+linterFix:
+	npx eslint --fix .
